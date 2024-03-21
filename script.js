961a045afa88a249cf7472d4356f3f69481c1b2a
Atith794 <68183452+Atith794@users.noreply.github.com>
2024-03-21 06:49:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // write js code here if required
+let timer = document.getElementById("timer");
+
+let time = new Date();
+let year = time.getFullYear();
+// console.log(time);
+let month = time.getMonth();
+let day = time.getDay();
+let hours = time.getHours();
+let minutes = time.getMinutes();
+let seconds = time.getSeconds();
+
+timer.innerHTML = `${day}/${month}/${year}, ${hours}:${minutes}:${seconds}`;
